beb9998c6ed757fd123716007e0ce76703ddfe92
Raman Novikau <demoikk@gmail.com>
2021-03-28 15:19:16 +0300

    feat: add return words count param

--- src/resources/words/word.service.js
+++ src/resources/words/word.service.js
@@ -4,10 +4,10 @@ const getAll = async conditions => wordRepo.getAll(conditions);
 
 const getGroup = async conditions => wordRepo.getGroup(conditions);
 
-const get = async wordId => {
+const get = async (wordId, count) => {
   const word = await wordRepo.get(wordId);
-
-  return word;
+  word.sort(() => Math.random() - 0.5);
+  return word.slice(0, count);
 };
 
 module.exports = { getAll, get, getGroup };
