2d9facee57ccd90d29ec7aaf87c58f7b9c7c38dc
RamanNovikau <demoikk@gmail.com>
2021-04-13 09:25:59 +0300

    fix: remove unused vars

--- src/app.js
+++ src/app.js
@@ -28,7 +28,6 @@ const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
 
 app.use(helmet());
 app.use(cors());
-app.use(express.json());
 app.use(express.json({ limit: '50mb', extended: true }));
 app.use(
   express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 })
