efb56ea3f263dcb3c25207c1b827c034a1f8db51
RamanNovikau <demoikk@gmail.com>
2021-04-13 09:20:09 +0300

    feat: add limit for load

--- src/app.js
+++ src/app.js
@@ -29,8 +29,10 @@ const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb' }));
+app.use(express.json({ limit: '50mb', extended: true }));
+app.use(
+  express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 })
+);
 
 app.use('/files', express.static(path.join(__dirname, '../files')));
 
